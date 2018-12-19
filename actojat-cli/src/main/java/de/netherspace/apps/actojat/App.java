2a1d7253ff267846aeab921d7fbfd3b56fd96267
patrickp89 <patrickpreuss@netherspace.de>
2018-12-19 10:44:43 +0100

    style: all classes in the CLI runner module now adhere to Google's Checkstyle ruleset

--- actojat-cli/src/main/java/de/netherspace/apps/actojat/App.java
+++ actojat-cli/src/main/java/de/netherspace/apps/actojat/App.java
@@ -5,36 +5,41 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App {
 
-    public static void main(String[] args) {
-        if (args.length < 5) {
-            log.error("Not enough arguments!");
-            return;
-        }
-
-        String sourceFile = args[0];
-        String clazzName = args[1];
-        String basePackage = args[2];
-        String languageString = args[3];
-        boolean showGuiTree = Boolean.parseBoolean(args[4]);
-
-        Language language;
-        if (languageString.equalsIgnoreCase("cobol")) {
-            language = Language.COBOL;
-
-        } else if (languageString.equalsIgnoreCase("c")) {
-            language = Language.C;
-
-        } else {
-            log.error("The language '" + languageString + " is not supported!");
-            return;
-        }
-
-        //TODO: take multiple files as input
-
-        new CliRunnerImpl().run(sourceFile, clazzName, basePackage, language, showGuiTree);
+  /**
+   * The JVM's entry point...
+   *
+   * @param args CLI args
+   */
+  public static void main(String[] args) {
+    if (args.length < 5) {
+      log.error("Not enough arguments!");
+      return;
     }
 
-    public enum Language {
-        COBOL, C
+    String sourceFile = args[0];
+    String clazzName = args[1];
+    String basePackage = args[2];
+    String languageString = args[3];
+    boolean showGuiTree = Boolean.parseBoolean(args[4]);
+
+    Language language;
+    if (languageString.equalsIgnoreCase("cobol")) {
+      language = Language.COBOL;
+
+    } else if (languageString.equalsIgnoreCase("c")) {
+      language = Language.C;
+
+    } else {
+      log.error("The language '" + languageString + " is not supported!");
+      return;
     }
+
+    //TODO: take multiple files as input
+
+    new CliRunnerImpl().run(sourceFile, clazzName, basePackage, language, showGuiTree);
+  }
+
+  public enum Language {
+    COBOL, C
+  }
 }
--- actojat-cli/src/main/java/de/netherspace/apps/actojat/CliRunner.java
+++ actojat-cli/src/main/java/de/netherspace/apps/actojat/CliRunner.java
@@ -5,16 +5,17 @@ package de.netherspace.apps.actojat;
  */
 public interface CliRunner {
 
-    /**
-     * Performs the transpilation for a given source code file.
-     *
-     * @param sourceFile  The source code file
-     * @param clazzName   The desired class name
-     * @param basePackage The desired base package
-     * @param language    The chosen language
-     * @param showGuiTree If the parse tree should be displayed graphically
-     * @return true if everything was fine, false else
-     */
-    boolean run(String sourceFile, String clazzName, String basePackage, App.Language language, boolean showGuiTree);
+  /**
+   * Performs the transpilation for a given source code file.
+   *
+   * @param sourceFile  The source code file
+   * @param clazzName   The desired class name
+   * @param basePackage The desired base package
+   * @param language    The chosen language
+   * @param showGuiTree If the parse tree should be displayed graphically
+   * @return true if everything was fine, false else
+   */
+  boolean run(String sourceFile, String clazzName, String basePackage,
+              App.Language language, boolean showGuiTree);
 
 }
